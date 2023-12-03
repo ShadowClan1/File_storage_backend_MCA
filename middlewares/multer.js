580a136d05ce28386bc29bb3e2d5cffed499d6b2
ritik <ritiks786@gmail.com>
2023-12-03 21:29:14 +0530

    all things are live

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,22 +1,22 @@
 const multer = require('multer')
-const {S3} = require('aws-sdk')
-const multerS3 = require('multer-s3')
+// const {S3} = require('aws-sdk')
+// const multerS3 = require('multer-s3')
 
 
-const s3 = new S3({
-    credentials : {
-        accessKeyId : 'AKIA27EJ5WER65PMHVVG',
-        secretAccessKey : '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'
-    },
-    region : 'ap-south-1'
-})
+// const s3 = new S3({
+//     credentials : {
+//         accessKeyId : 'AKIA27EJ5WER65PMHVVG',
+//         secretAccessKey : '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'
+//     },
+//     region : 'ap-south-1'
+// })
 
-const storageS3 = multerS3({
-    s3, bucket : 'file-manager', acl :'public-read', contentType :multerS3.AUTO_CONTENT_TYPE ,
-    key :(req, file, cb) => {
-        cb(null, Date.now().toString() + '-' + file.originalname)
-    }
-})
+// const storageS3 = multerS3({
+//     s3, bucket : 'file-manager', acl :'public-read', contentType :multerS3.AUTO_CONTENT_TYPE ,
+//     key :(req, file, cb) => {
+//         cb(null, Date.now().toString() + '-' + file.originalname)
+//     }
+// })
 
 const storage = multer.diskStorage({
     destination : (req, file, cb)=>{
